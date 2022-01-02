a3946e443b7e8e4938b1bfdd6066231f8e163b98
Hezinahayhdjeiehe <96947547+Hezinahayhdjeiehe@users.noreply.github.com>
2022-01-02 08:15:15 +0700

    Add files via upload

---
+++ assets/preload/weeks/week7.hx
@@ -0,0 +1,22 @@
+{
+	"songs": [
+		["Example1", "dad", [146, 113, 253]],
+		["Example2", "dad", [146, 113, 253]],
+		["Example3", "dad", [146, 113, 253]]
+	],
+
+	"weekCharacters": [
+		"dad",
+		"bf",
+		"gf"
+	],
+	"weekBackground": "stage",
+
+	"storyName": "custom week",
+	"weekBefore": "tutorial",
+	"weekName": "Week 1",
+	"startUnlocked": true,
+
+	"hideStoryMode": false,
+	"hideFreeplay": false
+}

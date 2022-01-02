e9533a48fdc8080f0e113074661bb0668a7c7e12
Hezinahayhdjeiehe <96947547+Hezinahayhdjeiehe@users.noreply.github.com>
2022-01-02 18:13:46 +0700

    Add files via upload

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -59,7 +59,8 @@ class ChartingState extends MusicBeatState
 		'Hurt Note',
 		'GF Sing',
 		'No Animation', 
-		'instakill note'
+		'instakill note', 
+		'bullet_note'
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();

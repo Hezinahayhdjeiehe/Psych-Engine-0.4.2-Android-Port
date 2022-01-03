073815988813506962d379a239790e42cfefdb0d
Hezinahayhdjeiehe <96947547+Hezinahayhdjeiehe@users.noreply.github.com>
2022-01-03 20:30:41 +0700

    Update Drain.lua

--- assets/preload/custom_events/Drain.lua
+++ assets/preload/custom_events/Drain.lua
@@ -2,9 +2,9 @@ function onEvent(name, value1, value2)
 	drainValue = tonumber(value1);
 	curHealth = getProperty('health');
 		if drainValue == null then
-		drainValue = 0.02;
+		drainValue = 0.01;
 		else 
-		damageValue = 0.02 + drainValue;
+		damageValue = 0.09 + drainValue;
 		end
 		if name == 'Drain' then
 			if curHealth > damageValue then
